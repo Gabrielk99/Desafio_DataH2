133bf81539ceb4f8c6fdfebdd508999f07ece886
gabrielk99 <gabriel_100@hotmail.com>
2022-04-30 00:47:11 -0300

    fix

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <div className="App">
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router >
             <Routes>
                 <Route exact path='/' element={<Home/>}/>
             </Routes>
